83b81aa5e557789a6c469f0c250e986827f55585
aquilaneo <aquilaneo@gmail.com>
2022-03-18 15:09:57 +0900

    初期状態

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 
-const { myAPI } = window;
+const {myAPI} = window;
 
 export const App = () => {
-  const [count, setCount] = useState(0);
 
-  const onCountUp = () => setCount((count) => count + 1);
-  const onCountDown = () => setCount((count) => count - 1);
+	return (
+		<div>
 
-  useEffect(() => {
-    myAPI.update(count);
-  }, [count]);
-
-  return (
-    <div className="container">
-      <h1>{count}</h1>
-      <div>
-        <button onClick={onCountDown}>&#x25BC;</button>
-        <button onClick={onCountUp}>&#x25B2;</button>
-      </div>
-    </div>
-  );
+		</div>
+	);
 };
