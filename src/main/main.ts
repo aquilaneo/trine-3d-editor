841deba5131775ee64177ab71a43cddd28a2c1e1
aquilaneo <aquilaneo@gmail.com>
2022-03-30 15:17:12 +0900

    ファイル読み書きイベントを作成

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,30 +1,46 @@
 import path from 'path';
-import { BrowserWindow, app, ipcMain } from 'electron';
+import {BrowserWindow, app, ipcMain} from 'electron';
+import * as fs from "fs";
 
 if (process.env.NODE_ENV === 'development') {
-  const execPath =
-    process.platform === 'win32'
-      ? '../node_modules/electron/dist/electron.exe'
-      : '../node_modules/.bin/electron';
+	const execPath =
+		process.platform === 'win32'
+			? '../node_modules/electron/dist/electron.exe'
+			: '../node_modules/.bin/electron';
 
-  require('electron-reload')(__dirname, {
-    electron: path.resolve(__dirname, execPath),
-  });
+	require ('electron-reload') (__dirname, {
+		electron: path.resolve (__dirname, execPath),
+	});
 }
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+	const mainWindow = new BrowserWindow ({
+		webPreferences: {
+			preload: path.join (__dirname, 'preload.js'),
+		},
+	});
 
-  ipcMain.on('update-title', (_e, arg) => {
-    mainWindow.setTitle(`Electron React TypeScript: ${arg}`);
-  });
+	ipcMain.on ('update-title', (_e, arg) => {
+		mainWindow.setTitle (`Electron React TypeScript: ${arg}`);
+	});
 
-  mainWindow.loadFile('dist/index.html');
+	mainWindow.loadFile ('dist/index.html');
 };
 
-app.whenReady().then(createWindow);
-app.once('window-all-closed', () => app.quit());
+app.whenReady ().then (createWindow);
+app.once ('window-all-closed', () => app.quit ());
+
+
+// ========== テキストファイル読み込み ==========
+ipcMain.handle ("read-text-file", async (event, filePath: string) => {
+	const content = await fs.promises.readFile (filePath);
+	return content.toString ();
+});
+// ==========================================
+
+
+// ========== テキストファイル書き込み ==========
+ipcMain.handle ("write-text-file", async (event, filePath: string, content: string) => {
+	await fs.promises.writeFile (filePath, content);
+});
+// ==========================================
